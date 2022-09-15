85e81cb480cdcea42e57d30decc618b48a4fc825
Elias Matheus Oliveira <elias.matheus@rainforest.tech>
2022-09-15 15:15:35 -0300

    Finish first server version

---
+++ server/src/utils/convert-minutes-to-hour-string.ts
@@ -0,0 +1,12 @@
+// 18:00 -> 1000
+
+function pad(number: number) {
+  return String(number).padStart(2, '0');
+}
+
+export function convertMinutesToHourString(minutesAmount: number) {
+  const hours = Math.floor(minutesAmount / 60);
+  const minutes = minutesAmount % 60;
+
+  return `${pad(hours)}:${pad(minutes)}`;
+}
